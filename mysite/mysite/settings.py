37fa27403aa45eb5deedef82bb976ab20c4b468c
artem <budakov@voltsbattery.com>
2021-08-17 12:04:47 +0300

    added worker in "mysite/settings.py"

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'worker.apps.WorkerConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
